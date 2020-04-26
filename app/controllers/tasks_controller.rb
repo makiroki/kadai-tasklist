596b2a93ccdedda0deeef71a87a8f2cf083e53b0
H.M <wildwidew@yahoo.co.jp>
2020-04-26 04:26:36 +0000

    kadai9.7-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     if logged_in?
@@ -11,7 +10,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    set_task
+   correct_user
   end
 
   def new
@@ -31,11 +30,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
+    correct_user
   end
 
   def update
-    set_task
+    correct_user
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -47,26 +46,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_task
+    correct_user
     @task.destroy
-
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end
   
   private
-  def set_task
-  @task = Task.find(params[:id])
-  end
-
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
   def correct_user
-    @tasks = current_user.tasks.find_by(id: params[:id])
-    unless @tasks
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
       redirect_to root_url
     end
   end
